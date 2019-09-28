30c51ccf5af7a930aa199af1d4e4ceab8b3d2e38
pntwari <55207309+pntwari@users.noreply.github.com>
2019-09-28 00:29:38 -0400

    Pull stock data for company over a period
    Pulls stock data for company over a period of time using the Alpha Vantage API. The company used for the exercise is Tesla. 
    With the stock data for a week and the sentiment from tweets about the company, the group can correlate the two to help an investor make decisions regarding the stock price of the company.

---
+++ alpha_pull.py
@@ -0,0 +1,48 @@
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import csv
+from datetime import datetime 
+# Import TimeSeries class
+from alpha_vantage.timeseries import TimeSeries
+from alpha_key import key_def
+ 
+ALPHA_VANTAGE_API_KEY = key_def() 
+ 
+
+ts = TimeSeries(key=ALPHA_VANTAGE_API_KEY, output_format='pandas', indexing_type='integer')
+ 
+
+interday_data, meta_data = ts.get_daily_adjusted(symbol='NYSE:TSLA', outputsize='full')
+
+# Print the information of the data
+alldatesstr = interday_data['index']
+alldates = []
+
+def convert(s):
+  return datetime.strptime(s, '%Y-%m-%d')
+for idem in range(0,len(alldatesstr)):
+    #datetime.strptime('alldatesstr[idem]' , '%Y-%m-%d')
+  alldates.append(convert(alldatesstr[idem]))
+
+allclose = interday_data['4. close']
+#print(alldates)
+
+print(alldates[1])
+date1 = datetime(2019, 9, 16) 
+date2 = datetime(2019, 9, 20) 
+
+date = []
+close = []
+
+
+for idam in range(0,len(alldates)):
+  if ((alldates[idam]>=date1) and (alldates[idam]<=date2)):
+    date.append(alldates[idam])
+    close.append(allclose[idam])
+
+print('The date of interest are ',date)
+
+with open('tesla_stock_try.csv', 'w') as f:
+  writer = csv.writer(f, delimiter='\t')
+  writer.writerows(zip(date,close))
